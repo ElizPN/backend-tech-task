110b1252a94bf50641c6b95c41ba8f46e17ffa8f
ElizPN <pankevych.liza@gmail.com>
2023-04-27 15:17:37 +0200

    Revert "change port"
    This reverts commit a92e32db91fe1031503e5ee16e0253a8755ede7a.

--- app/src/server/index.js
+++ app/src/server/index.js
@@ -5,7 +5,7 @@ const app = require("./app");
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, error => {
 
