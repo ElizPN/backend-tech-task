745cb61592074218b6d132986918b5cec6fe1a58
ElizPN <pankevych.liza@gmail.com>
2023-04-27 15:49:47 +0200

    change port and api url

--- app/src/server/index.js
+++ app/src/server/index.js
@@ -1,18 +1,15 @@
-
 const http = require("http");
 
 const app = require("./app");
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
-server.listen(PORT, error => {
+server.listen(PORT, (error) => {
+  if (error) {
+    return console.log(error);
+  }
 
-    if (error) {
-        return console.log(error);
-    }
-
-    console.log("🚀 Server started on port " + PORT);
-
+  console.log("🚀 Server started on port " + PORT);
 });
